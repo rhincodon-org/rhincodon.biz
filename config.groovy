cce470b50e815883b9c17a52bfaf294c935f1da3
rhincodon-org <kota@rhincodon.org>
2018-04-07 10:25:09 +0900

    initial commit.

---
+++ config.groovy
@@ -0,0 +1,13 @@
+// The title of this document
+title = "rhincodon.biz"
+
+// The version of this document
+version = "1.0.0"
+
+// The path of source files directory
+//sourceDirectoryPath = "./md"
+
+// The path of directory to be outputted a document
+outputDirectoryPath = "/var/www/rhincodon.biz"
+
+homePage="README.md"
---
+++ pages.groovy
@@ -0,0 +1,1 @@
+"README.md"(hidden: true, showPageToc: false)
